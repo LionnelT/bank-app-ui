07710ac6397b15789ae4d1bfe133e5f2f504434d
LionnelT <lionneltsuro56@gmail.com>
2023-08-18 12:52:04 +0200

    splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:bank_app_ui/screens/splash_screen.dart';
+import 'package:bank_app_ui/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,26 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: const ColorScheme.dark().copyWith(primary: coral),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SplashScreen(),
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,73 @@
+import 'package:bank_app_ui/utils/assets.dart';
+import 'package:bank_app_ui/widgets/login_option_button.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color(0xFF432148), Color(0xFF213048)],
+            
+          ),
+        ),
+        child: SafeArea(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Text('Bank App', style: Theme.of(context).textTheme.displayLarge),
+              SizedBox(
+                child: Column(
+                  children: [
+                    const Spacer(),
+                    Text(
+                      'Sign in by',
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    const SizedBox(height: 33),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          LoginOptionButton(
+                            assetPath: faceIdImage,
+                            title: 'Face Id',
+                            onPressed: () {
+                          
+                            },
+                          ),
+                          LoginOptionButton(
+                            assetPath: pinCodeImage,
+                            title: 'Pin Code',
+                            onPressed: () {
+                        
+                            },
+                          ),
+                          LoginOptionButton(
+                            assetPath: otherImage,
+                            title: 'Other',
+                            onPressed: () {
+                          
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 30)
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
